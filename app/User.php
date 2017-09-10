bb4447bc1522d9804ee3e991b87021dc8c61f6c5
Chris Brown <drbyte@zen-cart.com>
2017-09-10 15:07:04 -0400

    Install spatie laravel-permissions package (trait+migration)

--- app/User.php
+++ app/User.php
@@ -4,10 +4,12 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
