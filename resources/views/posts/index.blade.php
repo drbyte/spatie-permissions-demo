77ad89b8d3de615a0f92acb1ef360d351f24f4cf
Pawel K <39682172+Pawel-IT@users.noreply.github.com>
2024-12-15 13:46:09 -0700

    Added link to posts for guest users from welcome page
    To demo how the permissions work for guest users.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 @can('edit all posts')
     <p>You have permission to [edit all posts]. Clicking Edit Post below will return a edit page</p>
 @else
-    <p>You do NOT have permission to [edit all posts]. Clicking edit below will return a 403 Error UNLESS you own the post</p>
+    <p>You do NOT have permission to [edit all posts]. Clicking edit below will return a 403 Error UNLESS you own the post. For guests they will get a redirect to login page</p>
 @endcan
 
 @foreach($posts as $p)
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,6 +80,10 @@
                     <a href="{{route('home')}}" class="">View Demo</a>
                 </p>
 
+                <p class="text-xl text-center btn btn-md btn-danger mt-6">
+                    <a href="{{route('post.index')}}" class="">View Posts As Guest User</a>
+                </p>
+
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
                         <div class="flex items-center gap-4">
