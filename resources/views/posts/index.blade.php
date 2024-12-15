d090df7fdd9633d6a1f21fb65f22c3b844359d77
Pawel K <39682172+Pawel-IT@users.noreply.github.com>
2024-12-14 17:17:10 -0700

    Improved views to make them easier to understand
    and navigate

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,3 +1,13 @@
+@can('edit articles')
+    <p>You have permission to [edit articles]. Clicking Edit Post below will return a edit page</p>
+@else
+    <p>You do NOT have permission to edit articles. Clicking edit below will return a 403 Error</p>
+@endcan
+
 @foreach($posts as $p)
-  <p>{{ $p->id }}. <a href="{{route('post.show', ['post' => $p->id])}}"> {{ $p->title }}</a></p>
+    <p>{{ $p->id }}. <a href="{{route('post.show', ['post' => $p->id])}}"> {{ $p->title }}</a> (<a href="{{route('post.edit', ['post' => $p->id])}}">Edit Post</a>)</p>
 @endforeach
+
+<p>
+    <a href="{{route('home')}}" class="">Back to Demo Home Page</a>
+</p>
